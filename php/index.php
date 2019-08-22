c8e7a77156550682afed4f2624dd1a39710ccf83
riquetta <37386067+riquetta@users.noreply.github.com>
2019-08-22 17:05:58 -0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,21 +1,19 @@
 <?php
-/*$servername = "10.243.176.70";
-$username = "mysqldbuser";
-$password = "LataMIveco2019!";*/
-
-$username = $_ENV['MYSQL_USER'];
-$password = $_ENV['MYSQL_PASS'];
-
-
-
-// Create connection
-//$conn = new mysqli($servername, $username, $password);
-$conn = new mysqli("mysql:host=mysql;dbname=blog", $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+$dbuser = $_ENV['MYSQL_USER'];
+$dbpass = $_ENV['MYSQL_PASS'];
+try {
+    $pdo = new PDO("mysql:host=mysql;dbname=blog", $dbuser, $dbpass);
+    $statement = $pdo->prepare("SELECT * FROM posts");
+    $statement->execute();
+    $posts = $statement->fetchAll(PDO::FETCH_OBJ);
+    
+    echo "<h2>Posts</h2>";
+    echo "<ul>";
+    foreach ($posts as $post ) {
+        echo "<li>".$post->title."</li>";
+    }
+    echo "</ul>";
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
 ?>
-
